ded5764b5ebba56d79ba42d73976e27aa8d70d20
Karim <2jh0926@naver.com>
2022-11-02 19:57:43 +0900

    Skill Instagram

---
+++ project_defence/Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    Button gameStartButton;
+    [SerializeField]
+    Button gameQuitButton;
+    //[SerializeField]
+    //Button SettingButton;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // 게임이 시작되면 타이틀 영상이 끝날 때까지 버튼 비활성화
+        gameStartButton.interactable = false;
+        gameQuitButton.interactable = false;
+
+        StartCoroutine("ButtonOn");
+    }
+
+    // 5 + 여유시간(0.5초)뒤 버튼 활성화 
+    private IEnumerator ButtonOn()
+    {
+        yield return new WaitForSeconds(5.5f);
+        gameStartButton.interactable = true;
+        gameQuitButton.interactable = true;
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("game");
+    }
+}
